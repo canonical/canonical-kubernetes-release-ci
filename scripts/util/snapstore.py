b9351b9f3d6e474e37acbc80c01e7e503a585ce9
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-01-23 12:41:43 +0100

    address formatting comments

--- scripts/util/snapstore.py
+++ scripts/util/snapstore.py
@@ -22,16 +22,18 @@ def info(snap_name):
     return json.loads(r.text)
 
 
-def track_exists(snap_name: str, track_name: str):
+def track_exists(snap_name: str, track_name: str) -> bool:
+    """Check if a track exists for a snap."""
     snap_info = info(snap_name)
-    for channel_data in snap_info["channel-map"]:
-        track = channel_data["channel"]["track"]
-        if track == track_name:
+    for channel_data in snap_info.get("channel-map", {}):
+        track = channel_data.get("channel", {}).get("track")
+        if track and track == track_name:
             return True
     return False
 
 
-def ensure_track(snap_name: str, track_name: str):
+def ensure_track(snap_name: str, track_name: str) -> None:
+    """Ensure a track exists for a snap. If it does not exist, create it."""
     LOG.info("Ensuring track: %s %s", snap_name, track_name)
     if not track_exists(snap_name, track_name):
         create_track(snap_name, track_name)
@@ -39,7 +41,8 @@ def ensure_track(snap_name: str, track_name: str):
         LOG.info("Track already exists: %s %s", snap_name, track_name)
 
 
-def create_track(snap_name: str, track_name: str):
+def create_track(snap_name: str, track_name: str) -> None:
+    """Create a track for a snap. Throws an exception if the track already exists."""
     LOG.info("Creating track: %s %s", snap_name, track_name)
 
     # Yes, the snap creation API is really at charmhub.io.
@@ -57,6 +60,11 @@ def create_track(snap_name: str, track_name: str):
 
 
 def get_charmhub_auth_macaroon() -> str:
+    """Get the charmhub macaroon from the environment.
+
+    This is used to authenticate with the charmhub API.
+    Will raise a ValueError if CHARMCRAFT_AUTH is not set or the credentials are malformed.
+    """
     # Auth credentials provided by "charmcraft login --export $outfile"
     creds_export_data = os.getenv("CHARMCRAFT_AUTH")
     if not creds_export_data:
@@ -64,4 +72,7 @@ def get_charmhub_auth_macaroon() -> str:
 
     str_data = base64.b64decode(creds_export_data).decode()
     auth = json.loads(str(str_data))
-    return auth["v"]
+    v = auth.get("v")
+    if not v:
+        raise ValueError("Malformed charmhub credentials")
+    return v
